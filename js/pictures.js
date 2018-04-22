16746c4f8d4872781a4cd8dce01a0fa15da9e721
Maksym Vorozhtsov <31190340+MaksymVorozhtsov@users.noreply.github.com>
2018-04-22 22:30:29 +0200

    no magic, no objects

--- js/pictures.js
+++ js/pictures.js
@@ -1,32 +1,23 @@
 'use strict';
 (function () {
+  var NUMBER_OF_PHOTOS = 25;
   var photos = [];
+  var MIN_LIKES_NUMBER = 15;
+  var MAX_LIKES_NUMBER = 200;
   var getRandom = function (min, max) {
     var randomLikes = Math.floor(Math.random() * (max - min)) + min;
     return randomLikes;
   };
-  var descriptionVersions = {
-    1: 'Тестим новую камеру!',
-    2: 'Затусили с друзьями на море',
-    3: 'Как же круто тут кормят',
-    4: 'Отдыхаем...',
-    5: 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-    6: 'Вот это тачка!'
-  };
+  var descriptionVersions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
   var getRandomDescription = function (min, max) {
     var randomDescriptionNum = Math.floor(Math.random() * (max - min)) + min;
     var randomDescription = descriptionVersions[randomDescriptionNum];
     return randomDescription;
   };
+  var MIN_DESCRIPTION_NUMBER = 1;
+  var MAX_DESCRIPTION_NUMBER = 6;
   var comments = [];
-  var commentsVersions = {
-    1: 'Всё отлично!',
-    2: 'В целом всё неплохо. Но не всё.',
-    3: 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    4: 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    5: 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    6: 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  };
+  var commentsVersions = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
   var getRandomComments = function (maxNum, minNum, maxVer, minVer) {
     var randomCommentsNum = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
     for (var i = 0; i < randomCommentsNum; i++) {
@@ -35,12 +26,16 @@
     }
     return comments;
   };
-  for (var i = 0; i < 25; i++) {
+  var MIN_COMMENT_TEXT_NUMBER = 1;
+  var MAX_COMMENT_TEXT_NUMBER = 6;
+  var MIN_COMMENT_NUMBER = 1;
+  var MAX_COMMENT_NUMBER = 2;
+  for (var i = 0; i < NUMBER_OF_PHOTOS; i++) {
     photos[i] = {
       url: 'photos/' + (i + 1) + '.jpg',
-      likes: getRandom(15, 200),
-      comments: getRandomComments(1, 3, 1, 6),
-      description: getRandomDescription(1, 6)
+      likes: getRandom(MIN_LIKES_NUMBER, MAX_LIKES_NUMBER),
+      comments: getRandomComments(MIN_COMMENT_NUMBER, MAX_COMMENT_NUMBER, MIN_COMMENT_TEXT_NUMBER, MAX_COMMENT_TEXT_NUMBER),
+      description: getRandomDescription(MIN_DESCRIPTION_NUMBER, MAX_DESCRIPTION_NUMBER)
     };
   }
   var pictureElement = document.querySelector('.pictures');
