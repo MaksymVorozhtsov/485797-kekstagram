0afe9a97193126202d78658924504c20d778f7a7
Maksym Vorozhtsov <31190340+MaksymVorozhtsov@users.noreply.github.com>
2018-04-23 22:34:55 +0200

    getRandom + fragment

--- js/pictures.js
+++ js/pictures.js
@@ -1,41 +1,41 @@
 'use strict';
 (function () {
   var NUMBER_OF_PHOTOS = 25;
-  var photos = [];
   var MIN_LIKES_NUMBER = 15;
   var MAX_LIKES_NUMBER = 200;
-  var getRandom = function (min, max) {
-    var randomLikes = Math.floor(Math.random() * (max - min)) + min;
-    return randomLikes;
-  };
-  var descriptionVersions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
-  var getRandomDescription = function (min, max) {
-    var randomDescriptionNum = Math.floor(Math.random() * (max - min)) + min;
-    var randomDescription = descriptionVersions[randomDescriptionNum];
-    return randomDescription;
-  };
+  var DESCRIPTION_VERSIONS = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
   var MIN_DESCRIPTION_NUMBER = 1;
   var MAX_DESCRIPTION_NUMBER = 6;
-  var comments = [];
-  var commentsVersions = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-  var getRandomComments = function (maxNum, minNum, maxVer, minVer) {
-    var randomCommentsNum = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
-    for (var i = 0; i < randomCommentsNum; i++) {
-      var randomCommentsVersion = Math.floor(Math.random() * (maxVer - minVer)) + minVer;
-      comments[i] = commentsVersions[randomCommentsVersion];
-    }
-    return comments;
-  };
+  var COMMENTS_VERSIONS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
   var MIN_COMMENT_TEXT_NUMBER = 1;
   var MAX_COMMENT_TEXT_NUMBER = 6;
   var MIN_COMMENT_NUMBER = 1;
   var MAX_COMMENT_NUMBER = 2;
+  var AVATAR_MIN_NUM = 1;
+  var AVATAR_MAX_NUM = 6;
+  var photos = [];
+  var getRandom = function (min, max) {
+    var randomNum = Math.floor(Math.random() * (max - min)) + min;
+    return randomNum;
+  };
+  var comments = [];
+  var getRandomComments = function (randomCommentsNum, randomCommentsVersion) {
+    for (var i = 0; i < randomCommentsNum; i++) {
+      comments[i] = COMMENTS_VERSIONS[randomCommentsVersion];
+    }
+    return comments;
+  };
   for (var i = 0; i < NUMBER_OF_PHOTOS; i++) {
+    var randomDescriptionNum = getRandom(MIN_DESCRIPTION_NUMBER, MAX_DESCRIPTION_NUMBER);
+    var randomDescription = DESCRIPTION_VERSIONS[randomDescriptionNum];
+    var randomCommentsNum = getRandom(MIN_COMMENT_NUMBER, MAX_COMMENT_NUMBER);
+    var randomCommentsVersion = getRandom(MIN_COMMENT_TEXT_NUMBER, MAX_COMMENT_TEXT_NUMBER);
+    comments = getRandomComments(randomCommentsNum, randomCommentsVersion);
     photos[i] = {
       url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandom(MIN_LIKES_NUMBER, MAX_LIKES_NUMBER),
-      comments: getRandomComments(MIN_COMMENT_NUMBER, MAX_COMMENT_NUMBER, MIN_COMMENT_TEXT_NUMBER, MAX_COMMENT_TEXT_NUMBER),
-      description: getRandomDescription(MIN_DESCRIPTION_NUMBER, MAX_DESCRIPTION_NUMBER)
+      comments: comments,
+      description: randomDescription
     };
   }
   var pictureElement = document.querySelector('.pictures');
@@ -52,14 +52,13 @@
   bigPictureElement.querySelector('.likes-count').textContent = photos[0].likes;
   bigPictureElement.querySelector('.comments-count').textContent = photos[0].comments.length;
   var commentElement = document.querySelector('.social__comments');
-  var similarCommentsTemplate = document.querySelector('#comment').content;
-  var AVATAR_MIN_NUM = 1;
-  var AVATAR_MAX_NUM = 6;
+  var fragment = document.createDocumentFragment();
   for (var z = 0; z < photos[0].comments.length; z++) {
-    var newComment = similarCommentsTemplate.cloneNode(true);
-    newComment.querySelector('.social__comment span').textContent = photos[0].comments[z];
-    var getAvatarNumber = Math.floor(Math.random() * (AVATAR_MAX_NUM - AVATAR_MIN_NUM)) + AVATAR_MIN_NUM;
-    newComment.querySelector('.social__picture').src = 'img/avatar-' + getAvatarNumber + '.svg';
-    commentElement.appendChild(newComment);
+    var getAvatarNumber = getRandom(AVATAR_MAX_NUM, AVATAR_MIN_NUM);
+    var newComment = document.createElement('li');
+    newComment.className = 'social__comment social__comment--text';
+    newComment.innerHTML = '<img class="social__picture" src="img/avatar-' + getAvatarNumber + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">' + photos[0].comments[z];
+    fragment.appendChild(newComment);
   }
+  commentElement.appendChild(fragment);
 }());
