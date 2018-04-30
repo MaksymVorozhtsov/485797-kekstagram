432968727347d840e8e4cfa33377de7e7cf4dda8
Maksym Vorozhtsov <31190340+MaksymVorozhtsov@users.noreply.github.com>
2018-04-30 17:57:59 +0200

    delete returns

--- js/functions.js
+++ js/functions.js
@@ -4,11 +4,9 @@
   window.ENTER_KEYCODE = 13;
   window.popUpOpen = function (element, elementClass) {
     element.classList.remove(elementClass);
-    return popUpOpen;
   };
   window.popUpClose = function (element, elementClass) {
     element.classList.add(elementClass);
-    return popUpClose;
   };
   window.getRandom = function (min, max) {
     var randomNum = Math.floor(Math.random() * (max - min)) + min;
