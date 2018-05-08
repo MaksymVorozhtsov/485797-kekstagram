83d16f03d16c63fdd1ad6ac2252f89f675e3f337
Maksym Vorozhtsov <31190340+MaksymVorozhtsov@users.noreply.github.com>
2018-05-08 21:32:23 +0200

    отмена Б27

--- js/pictures.js
+++ js/pictures.js
@@ -39,9 +39,6 @@
         });
         commentElement.appendChild(commentFragment);
         window.functions.popUpOpen(bigPictureElement, 'visually-hidden');
-        clickablePhoto.removeEventListener('click', function () {
-          bigPictureRender(p);
-        });
       };
       clickablePhotos.forEach(function (clickablePhoto, p) {
         clickablePhoto.addEventListener('click', function () {
